0babd68b77d559f71434b1c1af92b33273978007
Pluto <thyagohyung@gmail.com>
2021-01-27 19:28:51 -0300

    Maratona Discover Dia 2 2.0

---
+++ script.js
@@ -0,0 +1,9 @@
+// Objeto Construtor ?
+const Modal = {
+   open() {
+      document.querySelector(".modal-overlay").classList.add("active");
+   },
+   close() {
+      document.querySelector(".modal-overlay").classList.remove("active");
+   },
+};
