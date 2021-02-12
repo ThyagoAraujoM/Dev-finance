23bba7fa4ac3f62eaf324364a46a903146cee831
Thyago <thyagohyung@gmail.com>
2021-02-12 01:15:42 -0300

    Atualizando o css com namespace

--- funções.js
+++ funções.js
@@ -9,17 +9,17 @@ const Wallet = {
   open() {
     // Abre o modal se não estiver na "aba" total e modifica o min max do date dependendo do mês para n colocar o mês errado.
 
-    document.querySelector(".wallet-overlay").classList.add("active");
+    document.querySelector(".c-wallet-overlay").classList.add("is-active");
   },
   close() {
     // Fecha o modal e limpa os valores nele inseridos.
-    document.querySelector(".wallet-overlay").classList.remove("active");
+    document.querySelector(".c-wallet-overlay").classList.remove("is-active");
     Form.clearFields();
   },
 
   closeOut() {
     // Fecha se clicar fora do moda.
-    let modal = document.getElementById("wallet-overlay");
+    let modal = document.getElementById("c-wallet-overlay");
     modal.addEventListener("click", function (e) {
       if (e.target == this) Wallet.close();
     });
